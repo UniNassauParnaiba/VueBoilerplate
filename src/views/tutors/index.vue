b0c9de3dbc70682499509e043b1e4ae3489b3935
LuizLins <luizmagao@gmail.com>
2025-10-22 19:19:15 -0300

    WIP: melhorando o index do tutores

--- src/views/tutors/index.vue
+++ src/views/tutors/index.vue
@@ -22,14 +22,14 @@
                 </label>
               </th>
               <th>Nome</th>
-              <th>Telefone</th>
               <th>Endereço</th>
+              <th>Telefone</th>
               <th>Ações</th>
             </tr>
           </thead>
           <tbody>
             <!-- row 1 -->
-            <tr>
+            <tr v-for="item in tutores" :key="item.id">
               <th>
                 <label>
                   <input type="checkbox" class="checkbox" />
@@ -46,7 +46,7 @@
                     </div>
                   </div>
                   <div>
-                    <div class="font-bold">Hart Hagerty</div>
+                    <div class="font-bold">{{ item.nome }}</div>
                     <div class="text-sm opacity-50">United States</div>
                   </div>
                 </div>
@@ -61,105 +61,6 @@
                 <button class="btn btn-ghost btn-xs">details</button>
               </th>
             </tr>
-            <!-- row 2 -->
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" class="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div class="flex items-center gap-3">
-                  <div class="avatar">
-                    <div class="mask mask-squircle h-12 w-12">
-                      <img
-                        src="https://img.daisyui.com/images/profile/demo/3@94.webp"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div class="font-bold">Brice Swyre</div>
-                    <div class="text-sm opacity-50">China</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Carroll Group
-                <br />
-                <span class="badge badge-ghost badge-sm">Tax Accountant</span>
-              </td>
-              <td>Red</td>
-              <th>
-                <button class="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            <!-- row 3 -->
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" class="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div class="flex items-center gap-3">
-                  <div class="avatar">
-                    <div class="mask mask-squircle h-12 w-12">
-                      <img
-                        src="https://img.daisyui.com/images/profile/demo/4@94.webp"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div class="font-bold">Marjy Ferencz</div>
-                    <div class="text-sm opacity-50">Russia</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Rowe-Schoen
-                <br />
-                <span class="badge badge-ghost badge-sm">Office Assistant I</span>
-              </td>
-              <td>Crimson</td>
-              <th>
-                <button class="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            <!-- row 4 -->
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" class="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div class="flex items-center gap-3">
-                  <div class="avatar">
-                    <div class="mask mask-squircle h-12 w-12">
-                      <img
-                        src="https://img.daisyui.com/images/profile/demo/5@94.webp"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div class="font-bold">Yancy Tear</div>
-                    <div class="text-sm opacity-50">Brazil</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Wyman-Ledner
-                <br />
-                <span class="badge badge-ghost badge-sm">Community Outreach Specialist</span>
-              </td>
-              <td>Indigo</td>
-              <th>
-                <button class="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
           </tbody>
           <!-- foot -->
         </table>
@@ -170,6 +71,48 @@
 
 <script setup>
 import breadcrumbs from "@/components/breadcrumbs.vue";
+
+const tutores = [
+  {
+    id: 1,
+    nome: "Luiz Lins",
+    endereco: {
+      cep: null,
+      numero: null,
+      logradouro: null,
+      complemento: null,
+      bairro: null,
+      cidade: null,
+    },
+    contato: ["(86)99969-2453", "(86)33225544"],
+  },
+  {
+    id: 2,
+    nome: "Maria Ires",
+    endereco: {
+      cep: null,
+      numero: null,
+      logradouro: null,
+      complemento: null,
+      bairro: null,
+      cidade: null,
+    },
+    contato: ["(86)99969-0011"],
+  },
+  {
+    id: 3,
+    nome: "João Abilio",
+    endereco: {
+      cep: null,
+      numero: null,
+      logradouro: null,
+      complemento: null,
+      bairro: null,
+      cidade: null,
+    },
+    contato: ["(86)99969-0011"],
+  },
+];
 </script>
 
 <style lang="scss" scoped></style>
