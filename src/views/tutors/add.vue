3becd4e90c6abaad610430183029e3d86f7f66ef
LuizLins <luizmagao@gmail.com>
2025-10-24 18:58:42 -0300

    WIP: melhorando formulario e adicionando dados ao vue

--- src/views/tutors/add.vue
+++ src/views/tutors/add.vue
@@ -4,34 +4,57 @@
       <legend class="fieldset-legend">Adicionando tutores</legend>
 
       <label class="label">Nome</label>
-      <input type="text" class="input w-full" placeholder="Nome completo" />
+      <input
+        type="text"
+        class="input w-full"
+        placeholder="Nome completo"
+        v-model="form.nomeCompleto"
+      />
 
       <label class="label">Logradouro</label>
-      <input type="text" class="input w-full" placeholder="Logradouro" />
+      <input type="text" class="input w-full" placeholder="Logradouro" v-model="form.logradouro" />
 
       <label class="label">Número</label>
-      <input type="text" class="input w-full" placeholder="Número" />
+      <input type="text" class="input w-full" placeholder="Número" v-model="form.numero" />
 
       <label class="label">Bairro</label>
-      <input type="text" class="input w-full" placeholder="Bairro" />
+      <input type="text" class="input w-full" placeholder="Bairro" v-model="form.bairro" />
 
       <label class="label">CEP</label>
-      <input type="text" class="input w-full" placeholder="CEP" />
+      <input type="text" class="input w-full" placeholder="CEP" v-model="form.cep" />
 
       <label class="label">Complemento</label>
-      <input type="text" class="input w-full" placeholder="Complemento" />
+      <input
+        type="text"
+        class="input w-full"
+        placeholder="Complemento"
+        v-model="form.complemento"
+      />
 
       <label class="label">Cidade</label>
-      <input type="text" class="input w-full" placeholder="Cidade" />
+      <input type="text" class="input w-full" placeholder="Cidade" v-model="form.cidade" />
 
       <label class="label">Estado</label>
-      <input type="text" class="input w-full" placeholder="Estado" />
+      <input type="text" class="input w-full" placeholder="Estado" v-model="form.estado" />
 
       <button class="btn btn-neutral mt-4">Adicionar</button>
     </fieldset>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+
+const form = reactive({
+  nomeCompleto: "",
+  logradouro: "",
+  numero: "",
+  bairro: "",
+  cep: "",
+  complemento: "",
+  cidade: "",
+  estado: "",
+});
+</script>
 
 <style lang="scss" scoped></style>
