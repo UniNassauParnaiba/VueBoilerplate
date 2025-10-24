fa97472f79db3397ae396f82cdeb47fbfbc0c444
LuizLins <luizmagao@gmail.com>
2025-10-24 20:56:36 -0300

    WIP: adicionar tutores

--- src/views/tutors/add.vue
+++ src/views/tutors/add.vue
@@ -3,30 +3,75 @@
     <legend class="fieldset-legend">Adicionando Tutores</legend>
 
     <label class="label">Nome completo</label>
-    <input type="text" class="input w-full" placeholder="Nome completo" />
+    <input
+      type="text"
+      class="input w-full"
+      v-model="form.nomeCompleto"
+      placeholder="Nome completo"
+    />
 
     <label class="label">CEP</label>
-    <input type="text" class="input w-full" placeholder="CEP" />
+    <input type="text" class="input w-full" v-model="form.endereco.cep" placeholder="CEP" />
 
     <label class="label">Bairro</label>
-    <input type="text" class="input w-full" placeholder="Bairro" />
+    <input type="text" class="input w-full" v-model="form.endereco.bairro" placeholder="Bairro" />
 
     <label class="label">Número</label>
-    <input type="text" class="input w-full" placeholder="Número" />
+    <input type="text" class="input w-full" v-model="form.endereco.numero" placeholder="Número" />
 
     <label class="label">Logradouro</label>
-    <input type="text" class="input w-full" placeholder="Logradouro" />
-
-    <label class="label">Bairro</label>
-    <input type="text" class="input w-full" placeholder="Bairro" />
+    <input
+      type="text"
+      class="input w-full"
+      v-model="form.endereco.logradouro"
+      placeholder="Logradouro"
+    />
 
     <label class="label">Cidade</label>
-    <input type="text" class="input w-full" placeholder="Cidade" />
+    <input type="text" class="input w-full" v-model="form.endereco.cidade" placeholder="Cidade" />
 
-    <button class="btn btn-neutral mt-4">Adicionar</button>
+    <label class="label">Estado</label>
+    <input type="text" class="input w-full" v-model="form.endereco.estado" placeholder="Estado" />
+
+    <button class="btn btn-neutral mt-4" @click="adicionarTutor">Adicionar</button>
   </fieldset>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive, onMounted } from "vue";
+import Localbase from "localbase";
+
+let db = null;
+
+onMounted(() => {
+  db = new Localbase("clinica-veterinaria");
+});
+
+const form = reactive({
+  nomeCompleto: null,
+  endereco: {
+    cep: null,
+    bairro: null,
+    numero: null,
+    logradouro: null,
+    cidade: null,
+    estado: null,
+  },
+});
+
+function adicionarTutor() {
+  db.collection("tutores").add({
+    nome: form.nomeCompleto,
+    endereco: {
+      cep: form.endereco.cep,
+      bairro: form.endereco.bairro,
+      numero: form.endereco.numero,
+      logradouro: form.endereco.logradouro,
+      cidade: form.endereco.cidade,
+      estado: form.endereco.estado,
+    },
+  });
+}
+</script>
 
 <style lang="scss" scoped></style>
