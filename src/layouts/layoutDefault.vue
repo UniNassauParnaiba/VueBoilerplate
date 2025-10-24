85ec860eafcfb2b1776947f11707a1c0a7fba099
LuizLins <luizmagao@gmail.com>
2025-10-24 18:26:57 -0300

    Alterando o nome sidebar

--- src/layouts/layoutDefault.vue
+++ src/layouts/layoutDefault.vue
@@ -2,7 +2,7 @@
   <!-- Layout principal -->
   <div class="flex">
     <!-- Sidebar (Menu Lateral) -->
-    <div class="w-64" v-show="slidebarAberto">
+    <div class="w-64" v-show="sidebarAberto">
       <logo />
       <menu-lateral />
     </div>
@@ -10,7 +10,7 @@
     <!-- Conteúdo Principal -->
     <div class="flex flex-col flex-1 overflow-hidden">
       <!-- Navbar Superior -->
-      <nav-bar v-model:estaAberto="slidebarAberto" />
+      <nav-bar v-model:estaAberto="sidebarAberto" />
 
       <!-- Área de Conteúdo (Rotas) -->
       <main class="flex-1 overflow-y-auto p-6">
@@ -26,9 +26,11 @@ import { RouterView } from "vue-router";
 import menuLateral from "@/components/menuLateral.vue";
 import logo from "@/components/logo.vue";
 import navBar from "@/components/navBar.vue";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
-const slidebarAberto = ref(false);
+const sidebarAberto = ref(true);
+
+onMounted(() => {});
 </script>
 
 <style lang="scss" scoped></style>
