11b3d3a29fa4a66249a3881a238e9bd2d5d59473
LuizLins <luizmagao@gmail.com>
2025-10-24 18:44:47 -0300

    WIP: criado formulario de tutores em adicionar

--- src/views/tutors/add.vue
+++ src/views/tutors/add.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>add.html</div>
+  <div class="w-full">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
+      <legend class="fieldset-legend">Adicionando tutores</legend>
+
+      <label class="label">Nome</label>
+      <input type="text" class="input w-full" placeholder="Nome completo" />
+
+      <label class="label">Logradouro</label>
+      <input type="text" class="input w-full" placeholder="Logradouro" />
+
+      <label class="label">Número</label>
+      <input type="text" class="input w-full" placeholder="Número" />
+
+      <label class="label">Bairro</label>
+      <input type="text" class="input w-full" placeholder="Bairro" />
+
+      <label class="label">CEP</label>
+      <input type="text" class="input w-full" placeholder="CEP" />
+
+      <label class="label">Complemento</label>
+      <input type="text" class="input w-full" placeholder="Complemento" />
+
+      <label class="label">Cidade</label>
+      <input type="text" class="input w-full" placeholder="Cidade" />
+
+      <label class="label">Estado</label>
+      <input type="text" class="input w-full" placeholder="Estado" />
+
+      <button class="btn btn-neutral mt-4">Adicionar</button>
+    </fieldset>
+  </div>
 </template>
 
 <script setup></script>
