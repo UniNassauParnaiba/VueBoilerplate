3e9b41bfb632f595116fbb5b5bc1b5efecf2840d
LuizLins <luizmagao@gmail.com>
2025-11-11 19:42:53 -0300

    feat: limpando uma string no model

--- src/models/Tutor.js
+++ src/models/Tutor.js
@@ -2,7 +2,7 @@
 export default class Tutor {
   constructor(chave, nomeCompleto, endereco) {
     this.id = chave || null;
-    this.nomeCompleto = nomeCompleto.toUpperCase();
+    this.nomeCompleto = nomeCompleto.toUpperCase().trim();
     this.endereco = endereco;
   }
 }
