4ddd862c3c200235c317c64fbef2aef66b2980a6
danielthomazz <danielgodawp@gmail.com>
2025-11-11 19:53:51 -0300

    mudando barra lateral

--- src/components/menuLateral.vue
+++ src/components/menuLateral.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="mx-3">
     <ul class="menu bg-base-200 rounded-box w-56">
-      <li><router-link to="tutors">Tutores</router-link></li>
-      <li><a>Menu</a></li>
+      <li><router-link to="cardapio">Cardápio</router-link></li>
+      <li><a>suporte</a></li>
     </ul>
   </div>
 </template>
